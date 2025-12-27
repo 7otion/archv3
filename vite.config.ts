86de97bc24baa14e429702ba9ba89edfdef76bc4
ev3nst <futuregnr@gmail.com>
2025-12-27 09:45:49 +0300

    tauri installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,38 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-	tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [
+		react({
+			babel: {
+				plugins: [['babel-plugin-react-compiler']],
+			},
+		}),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	clearScreen: false,
+	server: {
+		port: 5137,
+		strictPort: true,
+		host: host || false,
+		hmr: host
+			? {
+					protocol: 'ws',
+					host,
+					port: 5138,
+				}
+			: undefined,
+		watch: {
+			ignored: ['**/src-tauri/**'],
+		},
+	},
+});
