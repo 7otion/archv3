0845d0fca292c82cb630c6a2f4b4481cd98da00f
ev3nst <futuregnr@gmail.com>
2025-12-27 20:47:34 +0300

    orm initialization in app

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Router } from 'wouter';
+import {
+	ORM,
+	SQLiteDialect,
+	TauriAdapter,
+} from '7otion-orm';
 
 import { ErrorBoundary } from '@/components/error-boundary';
 import { Loading } from '@/components/loading';
@@ -8,6 +13,32 @@ import { Toaster } from '@/components/sonner';
 import { AppRouter } from './app-router';
 
 function App() {
+	const [dbInitialized, setDbInitialized] = useState(false);
+
+	const initializeDatabase = async () => {
+		const tauriAdapter = new TauriAdapter({
+			database: 'sqlite:archv3.db',
+			debug: true,
+		});
+		await tauriAdapter.initialize();
+		ORM.initialize({
+			adapter: tauriAdapter,
+			dialect: new SQLiteDialect(),
+			enableWriteQueue: true,
+		});
+		setDbInitialized(true);
+	};
+
+	useEffect(() => {
+		(async () => {
+			await initializeDatabase();
+		})();
+	}, []);
+
+	if (!dbInitialized) {
+		return <Loading />;
+	}
+
 	return (
 		<ErrorBoundary>
 			<Toaster
