04e7dca7d2ec056e0b746453240d933cb5ded7e3
ev3nst <futuregnr@gmail.com>
2025-12-28 12:57:41 +0300

    missing toastError

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from 'clsx';
+import { toast } from 'sonner';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function toastError(error: any, msg?: string) {
+	try {
+		toast.error(String(error));
+	} catch (_e) {}
+	console.error(msg, error);
 }
