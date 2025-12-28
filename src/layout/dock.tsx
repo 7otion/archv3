d277fdde30f891c2a78696533709120383cec25a
ev3nst <futuregnr@gmail.com>
2025-12-28 13:00:45 +0300

    layout dock integrated with content type docked prop

--- src/layout/dock.tsx
+++ src/layout/dock.tsx
@@ -1,9 +1,21 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
+import { useShallow } from 'zustand/react/shallow';
 import { DownloadIcon, HomeIcon } from 'lucide-react';
 
 import { FloatingDock } from '@/components/floating-dock';
+import { useContentTypesStore } from '@/lib/store/content-types';
 
 export function Dock() {
+	const [contentTypes, fetch] = useContentTypesStore(
+		useShallow(state => [state.items, state.fetch]),
+	);
+
+	useEffect(() => {
+		fetch();
+	}, [fetch]);
+
+	const dockedContentTypes = contentTypes.filter(ct => ct.docked === 1);
+
 	const links = useMemo(() => {
 		const homeLink = {
 			title: 'Home',
@@ -11,14 +23,27 @@ export function Dock() {
 			href: '/',
 		};
 
+		const dockedLinks = dockedContentTypes.map(ct => ({
+			title: ct.name,
+			icon: ct.icon ? (
+				<div
+					className="size-4 text-neutral-300"
+					dangerouslySetInnerHTML={{ __html: ct.icon }}
+				/>
+			) : (
+				<HomeIcon className="text-neutral-300" />
+			),
+			href: '/content-types/' + ct.slug,
+		}));
+
 		const downloaderLink = {
 			title: 'Downloader',
 			icon: <DownloadIcon className="text-neutral-300" />,
 			href: '/video-downloader',
 		};
 
-		return [homeLink, downloaderLink];
-	}, []);
+		return [homeLink, ...dockedLinks, downloaderLink];
+	}, [dockedContentTypes]);
 
 	return (
 		<div className="fixed inset-x-0 bottom-6 z-10 flex items-center justify-center">
