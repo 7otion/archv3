7c24831557be3884761eefb88562e1c50b85aeaf
ev3nst <futuregnr@gmail.com>
2025-12-28 19:24:39 +0300

    alert-dialog import fix

--- src/components/alert-dialog.tsx
+++ src/components/alert-dialog.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
 
+import { buttonVariants } from '@/lib/constants';
 import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/button';
 
 function AlertDialog({
 	...props
